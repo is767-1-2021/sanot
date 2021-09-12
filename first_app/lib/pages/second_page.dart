5dae63a6c5ed67a48d0a1005691a01210a9980b6
Sanot Suknet <sanot-suk63@tbs.tu.ac.th>
2021-09-12 23:38:55 +0700

    Forn+onSaved LauncherIcon TeamApp

---
+++ first_app/lib/pages/second_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.build_sharp),
+        onPressed: () {},
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello...World...',
+            ),
+            Table(
+              children: [
+                TableRow(children: [
+                  Container(
+                    child: Center(child: Text('No')),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  Container(
+                    child: Center(child: Text('Name')),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  Container(
+                    child: Center(child: Text('Gender')),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ]),
+                TableRow(
+                    children: [Text('1'), Text('Sanot Suknet'), Text('Male')]),
+                TableRow(children: [
+                  Text('2'),
+                  Text('Barack Obama'),
+                  Text('Male'),
+                ]),
+                TableRow(
+                    children: [Text('3'), Text('Donald Trump'), Text('Male')]),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
